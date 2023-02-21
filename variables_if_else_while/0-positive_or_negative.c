44d1848ab724cfd7327c39e0859dca7a00e707ab
root <darylfjw@hotmail.com>
2023-02-20 20:38:40 -0800

    fixed Q0 Betty code and doc style

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 
-/* Main - Prints a random number and states whether it is positive, negative or zero. 
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
  *
  * Return: Always 0.
 */
