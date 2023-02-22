d513c1a93fcfd33a06b303cbc7ca1c92452bbd55
root <darylfjw@hotmail.com>
2023-02-22 00:16:05 -0800

    fixed 7&9 into betty coding style

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -5,13 +5,14 @@
  *
  * Return: Always 0.
  */
+int main(void)
+{
+	char letter;
 
-int main(void) {
-    int letter = 'z';
-    while (letter >= 'a') {
-        putchar(letter);
-        letter--;
-    }
-    putchar('\n');
-    return 0;
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
 }
