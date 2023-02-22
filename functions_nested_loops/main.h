d08fd7acb80585fd980709ae391830759bf6daf3
root <darylfjw@hotmail.com>
2023-02-22 08:15:53 -0800

    Updated main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef main.h
-#define main.h
+#ifndef main_h
+#define main_h
 int _putchar(char c);
 int _putchar(char c);
 void print_alphabet(void);
@@ -14,4 +14,4 @@ void times_table(void);
 void print_times_table(int n);
 int add(int, int);
 void print_to_98(int n);
-
+#endif
